842fa84975dfe20e5215428c2cac0998f10146d5
Anna Var <juuni24@gmail.com>
2023-07-24 00:50:37 +0200

    Initial commit

--- src/test/java/ru/netology/services/FreelancerServiceTest.java
+++ src/test/java/ru/netology/services/FreelancerServiceTest.java
@@ -2,8 +2,6 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
 
 class FreelancerServiceTest {
 
